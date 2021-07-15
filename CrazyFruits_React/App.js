3ffb9187789606906deb475733f33f1cfbe11b0a
Henry <eplanas@edu.tecnocampus.cat>
2021-07-15 02:59:01 +0200

    load fonts

--- CrazyFruits_React/App.js
+++ CrazyFruits_React/App.js
@@ -1,31 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerWrapper}>
-        <View style={styles.userWrapper}>
-          <Image source={require('./assets/images/fotoLinkedIn.jpg')} style={styles.profileImage}></Image>
-          <Text style={styles.userName}>Enric Planas Mulet</Text>
+
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async loadFonts() {
+    await Font.loadAsync({
+      SFProRoundedRegular: require('./assets/fonts/SF-Pro-Rounded-Regular.otf'),
+      SFProRoundedBold: require('./assets/fonts/SF-Pro-Rounded-Bold.otf'),
+      SFProRoundedLight: require('./assets/fonts/SF-Pro-Rounded-Light.otf'),
+      SFProTextRegular: require('./assets/fonts/SF-Pro-Text-Regular.otf'),
+      SFProTextMedium: require('./assets/fonts/SF-Pro-Text-Medium.otf'),
+    });
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.headerWrapper}>
+          <View style={styles.userWrapper}>
+            <Image source={require('./assets/images/fotoLinkedIn.jpg')} style={styles.profileImage}></Image>
+            <Text style={styles.userName}>Enric Planas Mulet</Text>
+          </View>
+          <TouchableOpacity style={styles.settingsButton}>
+            <Image source={require('./assets/images/settings.png')} style={styles.settingsImage}></Image>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.settingsButton}>
-          <Image source={require('./assets/images/settings.png')} style={styles.settingsImage}></Image>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.buyBoxWrapper}>
-        <View style={styles.titleWrapper}>
-          <Text style={styles.buyBoxTitle}>Buy a new box</Text>
-          <Image source={require('./assets/images/emptyBox.png')} style={styles.buyBoxImage}></Image>
+        <View style={styles.buyBoxWrapper}>
+          <View style={styles.titleWrapper}>
+            <Text style={styles.buyBoxTitle}>Buy a new box</Text>
+            <Image source={require('./assets/images/emptyBox.png')} style={styles.buyBoxImage}></Image>
+          </View>
+          <Text style={styles.shopSelectorTitle}>Select a shop</Text>
         </View>
-        <Text style={styles.shopSelectorTitle}>Select a shop</Text>
+
+
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    )
+  }
 }
+const options = [{ value: 'LLIMONA', label: 'Lemon' },
+{ value: 'POMELO', label: 'Pomelo' },
+{ value: 'TARONJA', label: 'Orange' },
+{ value: 'PINYA', label: 'Pineapple' },
+{ value: 'MADUIXA', label: 'Strawberry' },
+{ value: 'MANDARINA', label: 'Tangerin' },
+{ value: 'MANGO', label: 'Mango' },
+{ value: 'PRÉSSEC', label: 'Peach' },
+{ value: 'ALBERCOC', label: 'Apricot' },
+{ value: 'CIRERA', label: 'Cherry' },
+{ value: 'POMA', label: 'Apple' },
+{ value: 'MELÓ', label: 'Melon' },
+{ value: 'AMETLLA', label: 'Almond' },
+{ value: 'AVELLANA', label: 'Hazlenut' },
+{ value: 'COCO', label: 'Coconut' },
+{ value: 'NOU', label: 'Nut' },
+];
+
 
 const styles = StyleSheet.create({
   container: {
@@ -50,9 +93,8 @@ const styles = StyleSheet.create({
   },
   userName: {
     marginLeft: 10,
-    fontFamily: "SF Pro Rounded",
+    fontFamily: "SFProRoundedRegular",
     fontSize: 17,
-    fontWeight: "300",
   },
   settingsButton: {},
   settingsImage: {
@@ -68,20 +110,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   buyBoxTitle: {
-    fontFamily: "SF Pro Rounded",
+    fontFamily: 'SFProRoundedBold',
     fontSize: 34,
-    fontWeight: "bold",
   },
   buyBoxImage: {
-    marginTop:6,
-    marginLeft:10,
+    marginTop: 6,
+    marginLeft: 10,
     width: 25,
     height: 25
   },
-  shopSelectorTitle:{
-    paddingTop:15,
-    fontFamily: "SF Pro Rounded",
+  shopSelectorTitle: {
+    paddingTop: 15,
+    fontFamily: "SFProRoundedRegular",
     fontSize: 18,
-    fontWeight: "500",
   },
 });
